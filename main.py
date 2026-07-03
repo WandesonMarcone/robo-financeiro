4fa5854e95f8bcf4e0b6bb5383baded4cf780b2e
cacec-stack <caci36bimec@gmail.com>
2026-07-03 10:25:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -131,8 +131,8 @@ def atualizar_financeiro():
         elif precisa_atualizar(ticker_c3, mapa_atualizacao, agora_dt, sp_tz) and ticker_c3 not in cat_fixas:
             cat_metodologia = [ticker_c3]
 
-    # --- 2.3 Oportunidades Reais ---
-    opps_brutas = df_filtros[(df_filtros['P/L'] > 0.1) & (df_filtros['P/L'] < 12) & (df_filtros['P/VP'] < 1.5)].index.tolist()
+    # --- 2.3 Oportunidades Reais --- PL MAIOR QUE 0 E MENOR QUE 12 / P/VP MENOR QUE 1,5
+    opps_brutas = df_filtros[(df_filtros['P/L'] > 0) & (df_filtros['P/L'] < 12) & (df_filtros['P/VP'] < 1.5)].index.tolist()
     # Pega apenas as que precisam de atualização e não estão nas categorias acima
     cat_opps = [o for o in opps_brutas if o in todas_originais and o not in cat_fixas and o not in cat_metodologia and precisa_atualizar(o, mapa_atualizacao, agora_dt, sp_tz)][:5]
 
@@ -268,7 +268,7 @@ def atualizar_financeiro():
         if cat_metodologia: 
             status_c3 = "(Adicionada na Planilha!)" if c3_nova else ""
             msg_wpp += f"🔍 *Metodologia (C3):*\n{', '.join(cat_metodologia)} {status_c3}\n\n"
-        if cat_aleatorias: msg_wpp += f"🎲 *Varredura de Desatualizadas:*\n{', '.join(cat_aleatorias)}\n\n"
+        if cat_aleatorias: msg_wpp += f"🎲 *Ações Desatualizadas:*\n{', '.join(cat_aleatorias)}\n\n"
         if relatorio_opps: msg_wpp += "🎯 *Ações em Oportunidade:*\n" + "\n".join(relatorio_opps) + "\n\n"
         if relatorio_novatas: msg_wpp += "🌟 *NOVA PREVIDENCIÁRIA ADICIONADA:*\n" + "\n".join(relatorio_novatas)
 
