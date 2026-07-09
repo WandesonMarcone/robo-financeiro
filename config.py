c7dd258a07bb958d86ee0b17ab44ab4c91f6b62f
WandesonMarcone <wandesonm07@gmail.com>
2026-07-09 19:10:34 -0300

    Atualizar o config.py

--- config.py
+++ config.py
@@ -10,6 +10,9 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 # O Chat ID pode ficar público, pois é apenas um endereço de destino e sem o Token ninguém consegue mandar mensagens para lá.
 TELEGRAM_CHAT_ID = "8867098987"
 
+# Adicione isso ao seu config.py
+DRIVE_FOLDER_ID = "1Q-dkO4oSd6_9zmOeZmPX8nmuVWzdjHOq"
+
 # --- PREFERÊNCIAS DE ATIVOS FIXOS (A SUA CARTEIRA DO CORAÇÃO) ---
 FIXAS_ACOES = ["PETR4", "VALE3", "ITUB4", "BBDC4"] 
 FIXAS_FIIS = ["GARE11", "MXRF11", "VISC11", "HGLG11"] # Pode alterar e colocar os seus FIIs reais aqui
