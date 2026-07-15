460caee9f771da9c09abc08f210f121135bf86d3
WandesonMarcone <wandesonm07@gmail.com>
2026-07-15 17:20:58 -0300

    Atualizar o bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,8 +7,6 @@ import os
 from flask import Flask, request
 from sqlalchemy import func, create_engine
 from sqlalchemy.orm import sessionmaker
-import dropbox # Necessário para tratar exceções da Logo
-
 import config
 from atualizador_documentos import rotina_de_atualizacao_em_massa
 from modules.utils import conectar_gspread
