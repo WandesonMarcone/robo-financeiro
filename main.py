fc1b6485ee5a93ab2b3e25489af4de238b6ac840
WandesonMarcone <wandesonm07@gmail.com>
2026-07-18 02:09:22 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import pytz
+import telebot
 from flask import Flask, request
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
