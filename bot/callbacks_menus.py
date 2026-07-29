6d7d0d643c5d1a30e1731c7465eda7c64c4cd5fb
cacec-stack <caci36bimec@gmail.com>
2026-07-29 15:01:05 -0300

    Update callbacks_menus.py

--- bot/callbacks_menus.py
+++ bot/callbacks_menus.py
@@ -489,6 +489,7 @@ def callback_geral(call):
 
                 ano_escolhido = data_ref.split('-')[0]
                 markup = InlineKeyboardMarkup()
+                markup.add(InlineKeyboardButton("❓ Entender os Indicadores", callback_data=f"ajuda_cvm_{ticker}_menu"))
                 markup.add(InlineKeyboardButton("🔙 Voltar aos Trimestres", callback_data=f"ano_{ticker}_{tipo_ativo}_{ano_escolhido}"))
                 markup.add(InlineKeyboardButton("🔙 Voltar ao Painel", callback_data=f"painel_{ticker}_{tipo_ativo}"))
 
